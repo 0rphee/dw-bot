575212faae4ad61fc7fe92bb3669393bd3d2f5ca
0rphee <0rph3e@proton.me>
2024-07-15 16:10:56 -0600

    vid 1 tutorial terminado

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { Client, IntentsBitField } = require("discord.js");
+const config = require("../config.json");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.login(config.token);
