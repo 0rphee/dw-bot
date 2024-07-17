a0a40500f089cf2bb1a11b64750c02c72aaf88a5
0rphee <0rph3e@proton.me>
2024-07-17 12:58:41 -0600

    vid 8 finished

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,11 +1,15 @@
 module.exports = {
   name: "ping",
-  description: "Pong!",
-  // devOnly: boolean,
-  // testOnly: boolean  ,
-  // options : Object[],
-  // deleted:
-  callback: (client, interaction) => {
-    interaction.reply(`Pong! ${client.ws.ping}ms`);
+  description: "Replies with the bot ping!",
+
+  callback: async (client, interaction) => {
+    await interaction.deferReply();
+
+    const reply = await interaction.fetchReply();
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+    interaction.editReply(
+      `Pong! Cient ${ping}ms | Websocket: ${client.ws.ping}ms`,
+    );
   },
 };
